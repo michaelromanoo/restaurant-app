39f9ad99cabfe25d0cf4301d21f2d1f988a93009
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-05-04 17:19:26 +0800

    feat: add font awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
+// import font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// import vuex
 import store from "./store/store";
 
+import Vue from "vue";
+import App from "./App.vue";
+
 Vue.config.productionTip = false;
+
+library.add(faCartShopping);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   store,
